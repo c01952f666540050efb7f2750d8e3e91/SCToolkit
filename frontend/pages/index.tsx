34030b0b1d2916bb3d5d5a2f9c9a8795d7aee818
c01952f666540050efb7f2750d8e3e91 <felix.voo@ledger.com>
2023-04-02 19:26:47 +0200

    adjusting landing page

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -10,6 +10,21 @@ export default function Home() {
       </Head>
       <main>
         <TopNavbar />
+        <br/>
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+          <Image src="/../public/home_image.jpeg" alt="Home" width={300} height={168} />
+        </div>
+        <br />
+        <div style={{display:'inline', alignItems: 'center', width:'50%'}}>
+          <p style={{textAlign:'center'}}>
+            With this tool, you can easily manage and organize your cryptocurrency addresses across multiple derivation paths. Whether you're using a hardware wallet or software wallet, this tool allows you to easily send and receive transactions from any of your addresses. Additionally, you can deploy and interact with smart contracts on various blockchain networks, giving you greater flexibility and control over your digital assets. So whether you're an experienced cryptocurrency user or just starting out, this tool has everything you need to manage and interact with your addresses and contracts in a simple and user-friendly interface.
+          </p>
+          <br />
+          <p style={{textAlign:'center'}}>
+            <b>Please use this at your own risk, as this was mainly used as a testing platform for myself.</b>
+          </p>
+          <br />
+        </div>
         
       </main>
     </>
