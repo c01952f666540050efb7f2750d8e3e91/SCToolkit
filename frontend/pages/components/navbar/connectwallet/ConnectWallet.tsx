8b008f4675bb2eb48436c67608b1e24e1cc9642e
c01952f666540050efb7f2750d8e3e91 <felix.voo@ledger.com>
2023-04-04 19:31:55 +0200

    it worked? lol

--- frontend/pages/components/navbar/connectwallet/ConnectWallet.tsx
+++ frontend/pages/components/navbar/connectwallet/ConnectWallet.tsx
@@ -1,12 +1,42 @@
 import { useState, useEffect } from 'react';
 import { Button } from '@nextui-org/react';
+import { useConnectWallet } from '@web3-onboard/react';
+import { createPublicClient, http } from 'viem';
+import { mainnet } from 'viem/chains';
+import { ethers } from 'ethers';
+// import { ether } from 'viem';
+
+// const client = createPublicClient({
+//     chain: mainnet,
+//     transport: http(),
+//   });
+  
+  // 3. Consume an action!
 
 
 
 export default function ConnectWallet() {
+    const [{ wallet, connecting }, connect, disconnect] = useConnectWallet()
+    // const [w3client, setW3Client] = useState<typeof createPublicClient | null>()
+    const [ethersProvider, setProvider] = useState<ethers.providers.Web3Provider | null>()
+
+    useEffect(() => {
+      // If the wallet has a provider than the wallet is connected
+        if (wallet?.provider) {
+        setProvider(new ethers.providers.Web3Provider(wallet.provider, 'any'))
+      // if using ethers v6 this is:
+        //  ethersProvider = new ethers.BrowserProvider(wallet.provider, 'any')
+    }
+    }, [wallet])
+
+    const handleConnect = async () => {
+        await connect();
+    };
 
     return (
-        <Button>
+        <Button
+            onClick={handleConnect}
+        >
         Connect
         </Button>
     )
